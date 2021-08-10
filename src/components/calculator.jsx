449fbe3587325fda46afea47e060621e41964222
moise10r <moiserushanika2006@gmail.com>
2021-08-10 12:45:23 +0200

    Add calculator signes

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -14,33 +14,32 @@ class Calculator extends Component {
         </div>
         <div className="row flex-center keyboard">
           <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
+          <button className="btn" type="button">-/+</button>
+          <button className="btn" type="button">&#x25;</button>
+          <button className="btn" type="button">&#xf7;</button>
         </div>
         <div className="row flex-center keyboard">
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
+          <button className="btn" type="button">7</button>
+          <button className="btn" type="button">8</button>
+          <button className="btn" type="button">9</button>
+          <button className="btn" type="button">x</button>
         </div>
         <div className="row flex-center keyboard">
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
+          <button className="btn" type="button">4</button>
+          <button className="btn" type="button">5</button>
+          <button className="btn" type="button">6</button>
+          <button className="btn" type="button">-</button>
         </div>
         <div className="row flex-center keyboard">
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
+          <button className="btn" type="button">1</button>
+          <button className="btn" type="button">2</button>
+          <button className="btn" type="button">3</button>
+          <button className="btn" type="button">+</button>
         </div>
         <div className="row flex-center keyboard">
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
-          <button className="btn" type="button">AC</button>
+          <button className="btn" type="button">0</button>
+          <button className="btn" type="button">.</button>
+          <button className="btn" type="button">=</button>
         </div>
       </div>
     );
