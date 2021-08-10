e380da503b62b97e7fc52510b6df0792b56331b9
moise10r <moiserushanika2006@gmail.com>
2021-08-10 13:44:17 +0200

    Transform App into class base component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import Calculator from './components/calculator';
 import './assets/styles/main.css';
 
-function App() {
-  return (
-    <div className="App flex-center">
-      <Calculator />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App flex-center">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
