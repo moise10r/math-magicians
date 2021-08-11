c358fd23bcd180aa2e213c7cc095239f38e0e218
moise10r <moiserushanika2006@gmail.com>
2021-08-11 18:22:26 +0200

    Add result  component

---
+++ src/components/common/result.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Result extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+    return (null);
+  }
+}
+
+export default Result;
