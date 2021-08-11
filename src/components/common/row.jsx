4d91586c3e4608fedccaa2f99d64e0df73d52861
moise10r <moiserushanika2006@gmail.com>
2021-08-11 16:52:54 +0200

    Add Row component

---
+++ src/components/common/row.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Row extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+    return (null);
+  }
+}
+
+export default Row;
