21dcfeed55b46dedbfa161176f337f867c911300
moise10r <moiserushanika2006@gmail.com>
2021-08-18 23:55:38 +0200

    Add quote style

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,11 +2,13 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './header';
 import CalculatorPage from './pages/calculatorPage';
 import Home from './pages/home';
+import Quote from './pages/quote';
 
 const Main = () => (
   <div className="main-home-container">
     <Header />
     <Switch>
+      <Route path="/quote" component={Quote} />
       <Route path="/calculator" component={CalculatorPage} />
       <Route path="/" exact component={Home} />
     </Switch>
