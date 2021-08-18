70d1b77ef1b8d932eec13d7ff69eef9ca521fed5
moise10r <moiserushanika2006@gmail.com>
2021-08-18 23:06:49 +0200

    Add calculator style

--- src/components/pages/calculatorPage.jsx
+++ src/components/pages/calculatorPage.jsx
@@ -1,3 +1,22 @@
-const CalculatorPage = () => (<p>Calcul</p>);
+import Calculator from '../Calculator';
+
+const CalculatorPage = () => (
+  <section className="calculator-main-section">
+    <div className="bg" />
+    <div className="calculator-main-wrapper">
+      <div className="calculator-container flex-between">
+        <div className="right-container">
+          <p>
+            Let do some Math
+            <span>!</span>
+          </p>
+        </div>
+        <div className="left-container">
+          <Calculator />
+        </div>
+      </div>
+    </div>
+  </section>
+);
 
 export default CalculatorPage;
