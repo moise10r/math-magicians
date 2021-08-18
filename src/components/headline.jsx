cf36b6d4aabc00c4472659e5753eb6f907d1e4c4
moise10r <moiserushanika2006@gmail.com>
2021-08-18 21:36:30 +0200

    Add the home page component

---
+++ src/components/headline.jsx
@@ -0,0 +1,22 @@
+const Headline = () => (
+  <section className="main-headline-section">
+    <div className="bg" />
+    <div className="main-headline-container flex-center">
+      <div className="main-wrapper">
+        <h2>
+          The Best
+          <span>Calculator</span>
+          Out There
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada
+          lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien.
+          Suspendisse cursus faucibus finibus.
+        </p>
+        <a href="#2">Read More</a>
+      </div>
+    </div>
+  </section>
+);
+
+export default Headline;
--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,8 +1,10 @@
 import Header from '../header';
+import Headline from '../headline';
 
 const Home = () => (
   <div className="main-home-container">
     <Header />
+    <Headline />
   </div>
 );
 
