cc36982ea4e03f85d7597c0ba72afa1bd57ed984
moise10r <moiserushanika2006@gmail.com>
2021-08-19 21:35:28 +0200

    Add the quote component test.

--- src/__test__/pages/home.test.js
+++ src/__test__/pages/home.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Home from '../components/pages/home';
+import Home from '../../components/pages/home';
 
-it('should match the snapshot', () => {
+it('should match the snapshot of Home', () => {
   const tree = renderer.create(<Home />).toJSON();
   expect(tree).toMatchSnapshot();
 });
