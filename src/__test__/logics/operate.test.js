4498253a76d330cda4b8f0d978834cd2d9fd4df9
moise10r <moiserushanika2006@gmail.com>
2021-08-20 12:01:00 +0200

    Fix js linter errors.

--- src/__test__/logics/operate.test.js
+++ src/__test__/logics/operate.test.js
@@ -1,28 +1,28 @@
 import operate from '../../logics/operate';
 
-describe('Operator functionality',() => {
- it ('Should return the summation of two numbers', () => {
-   const result = operate(5, 8, '+');
-   expect(result).toBe('13');
- });
+describe('Operator functionality', () => {
+  it('Should return the summation of two numbers', () => {
+    const result = operate(5, 8, '+');
+    expect(result).toBe('13');
+  });
 
- it ('Should return the difference of two numbers', () => {
-  const result = operate(8, 5, '-');
-  expect(result).toBe('3');
-});
+  it('Should return the difference of two numbers', () => {
+    const result = operate(8, 5, '-');
+    expect(result).toBe('3');
+  });
 
-it ('Should return the product of two numbers', () => {
- const result = operate(2, 5, 'x');
- expect(result).toBe('10');
-});
+  it('Should return the product of two numbers', () => {
+    const result = operate(2, 5, 'x');
+    expect(result).toBe('10');
+  });
 
-it ('Should return the quotient of two numbers', () => {
- const result = operate(10, 2, '÷');
- expect(result).toBe('5');
-});
+  it('Should return the quotient of two numbers', () => {
+    const result = operate(10, 2, '÷');
+    expect(result).toBe('5');
+  });
 
-it ('Should return the rest of two numbers', () => {
- const result = operate(8, 2, '%');
- expect(result).toBe('0');
-});
+  it('Should return the rest of two numbers', () => {
+    const result = operate(8, 2, '%');
+    expect(result).toBe('0');
+  });
 });
